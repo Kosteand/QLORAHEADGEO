3426c048da0309061f181f3c26eb3a79a16909c4
Andre Kostenko <ak2787@cornell.edu>
2026-05-02 04:14:11 -0400

    Added to verify pooling

--- scripts/overfit_test.py
+++ scripts/overfit_test.py
@@ -129,7 +129,8 @@ def main():
         per_device_eval_batch_size=8,
         gradient_accumulation_steps=2,
         num_train_epochs=3,
-        learning_rate=5e-4,
+        learning_rate=5e-5, 
+        max_grad_norm=1.0,
         lr_scheduler_type="constant",  # no decay - we want to keep pushing
         warmup_ratio=0.03,
         bf16=True,
